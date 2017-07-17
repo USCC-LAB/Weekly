ffc88a94cc5c435d491bf01b10c900241e7c19da
yenWu <c14006078@gmail.com>
2017-07-17 18:32:47 +0800

    [MAIL] impl mail interface

---
+++ mail/mail.py
@@ -0,0 +1,12 @@
+class mail:
+    def __init__(self, prot, *argv):
+        self.prot = prot(*argv)
+
+    def login(self, account, passwd):
+        self.prot.login(account, passwd)
+
+    def send(self, frm, to, subject, content):
+        self.prot.sendmail(frm, to, subject, content)
+
+    def quit(self):
+        self.prot.quit()
