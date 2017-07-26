0cdacd3648ca73e5703c5dcaecba0a674044dcfa
s8w1e2ep <ha9mv8c@gmail.com>
2017-07-26 18:40:49 +0800

    Expand send function

--- mail/mail.py
+++ mail/mail.py
@@ -5,8 +5,13 @@ class mail:
     def login(self, account, passwd):
         self.prot.login(account, passwd)
 
+<<<<<<< HEAD
     def send(self, frm, to, subject, content):
         self.prot.send(frm, to, subject, content)
+=======
+    def send(self, frm, to, subject, content, *attachments):
+        self.prot.send(frm, to, subject, content, attachments)
+>>>>>>> d3c9f75... Simplify send function and delete duplicate code
 
     def quit(self):
         self.prot.quit()
--- mail/smtp/smtp.py
+++ mail/smtp/smtp.py
@@ -16,8 +16,64 @@ class smtp:
     def login(self, account, passwd):
         self.smtp_obj.login(account, passwd)
 
+<<<<<<< HEAD
     def send(self, frm, to, subject, content):
         self.smtp_obj.sendmail(frm, to, 'Subject:' + subject + '\n' + content)
+=======
+    def send(self, frm, to, subject, content, *attachments):
+        # render msg
+        msg = MIMEMultipart()
+        msg['Subject'] = subject
+        msg['To'] = ', '.join(to)
+        msg['Date'] = str(date.today())
+        msg['From'] = frm
+        msg.attach(MIMEText(content))
+
+        for attachment in attachments:
+            try:
+                with open(attachment, 'rb') as fp:
+                    file = MIMEBase('application', "octet-stream")
+                    file.set_payload(fp.read())
+                encoders.encode_base64(file)
+                file.add_header('Content-Disposition', 'attachment',
+                filename=os.path.basename(attachment))
+                msg.attach(file)
+            except:
+                print("Unable to open one of the attachments. Error: ", sys.exc_info()[0])
+                raise
+        try:
+            self.smtp_obj.sendmail(frm, to, msg.as_string())
+            print ('Send email success.')
+        except:
+            print ('Send email Error: ', sys.exc_info()[0])
+
+    def send_with_attachment(self, frm, to, subject, content, attachments):
+        # render msg
+        msg = MIMEMultipart()
+        msg['Subject'] = subject
+        msg['To'] = ', '.join(to)
+        msg['Date'] = str(date.today())
+        msg['From'] = frm
+        msg.attach(MIMEText(content))
+
+        for attachment in attachments:
+            try:
+                with open(attachment, 'rb') as fp:
+                    file = MIMEBase('application', "octet-stream")
+                    file.set_payload(fp.read())
+                encoders.encode_base64(file)
+                file.add_header('Content-Disposition', 'attachment',
+                filename=os.path.basename(attachment))
+                msg.attach(file)
+            except:
+                print("Unable to open one of the attachments. Error: ", sys.exc_info()[0])
+                raise
+        try:
+            self.smtp_obj.sendmail(frm, to, msg.as_string())
+            print ('Send email with attachment success.')
+        except:
+            print ('Send with attachment Error: ', sys.exc_info()[0])
+>>>>>>> d3c9f75... Simplify send function and delete duplicate code
 
     def quit(self):
         self.smtp_obj.quit()
