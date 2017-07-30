2e8b7c0b447339be5f7bba5275cde1e11933320d
yenWu <c14006078@gmail.com>
2017-07-30 13:32:53 +0800

    [Unittest][Reader] threaded uid reader by ACR122

---
+++ tests/reader_test.py
@@ -0,0 +1,19 @@
+import unittest
+from tests.reader_testsuite.uid.threaded import *
+
+from reader.NFC.ACR122.ACR122 import *
+
+class ReaderTestcase(unittest.TestCase):
+    def setUp(self):
+        self.dev    = ACR122
+        self.delta  = 10
+
+    def tearDown(self):
+        self.dev    = None
+        self.delta  = None
+
+    def test_NFC_uid(self):
+        expected = 0
+        threaded_read_uid(self.dev, self.delta)
+        result = 0
+        self.assertEqual(expected, result)
