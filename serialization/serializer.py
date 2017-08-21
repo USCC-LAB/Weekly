169c50e0957287fd89b805cc35bff329c937a63c
莊雲凱 <as23041248@gmail.com>
2017-08-21 09:39:14 +0800

    [Serialization] Impl Serializer interface.

---
+++ serialization/serializer.py
@@ -0,0 +1,10 @@
+
+class Serializer:
+    def __init__(self, seralize_obj):
+        self.seralize_obj = seralize_obj
+
+
+    def seralize(self,seralize_obj , data):
+        return seralize_obj.seralize(data):
+    
+
