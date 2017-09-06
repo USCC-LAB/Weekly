c0e9ce09b117d2877603dd5f7b1b605251d07310
s8w1e2ep <ha9mv8c@gmail.com>
2017-09-06 17:41:44 +0800

    Simplify Mail class and Smtp class

--- mail/mail.py
+++ mail/mail.py
@@ -1,12 +1,17 @@
-class mail:
+class Mail:
+    """
+    This module is to send email by google smtp.
+    You can use this module to send email.
+    """
+
     def __init__(self, prot, *argv):
         self.prot = prot(*argv)
 
     def login(self, account, passwd):
         self.prot.login(account, passwd)
 
-    def send(self, frm, to, subject, content, *attachment):
-        self.prot.send(frm, to, subject, content, attachment)
+    def send(self, frm, to, content):
+        self.prot.send(frm, to, content)
 
     def quit(self):
         self.prot.quit()
--- mail/smtp/smtp.py
+++ mail/smtp/smtp.py
@@ -1,58 +1,29 @@
-import os
-import sys
 import smtplib
-from email import encoders
-from email.mime.base import MIMEBase
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from datetime import date
 
-class smtp:
-    # smtp_obj =
+
+class Smtp:
 
     def __init__(self, *argv):
         if () != argv:
             self.smtp_obj = smtplib.SMTP_SSL(*argv)
         else:
-            self.smtp_obj = self.default_setup()
-
-    # Set Gmail server as default
-    def default_setup(self):
-        return smtplib.SMTP_SSL('smtp.gmail.com', 465)
+            # Set Gmail server as default
+            self.smtp_obj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 
     def login(self, account, passwd):
+        """
+        You need login by gmail account.
+        """
         self.smtp_obj.login(account, passwd)
 
-    def send(self, frm, to, subject, content, *attachments):
-        msg = self.render_msg(frm, to, subject, content, attachments)
-        self.smtp_obj.sendmail(frm, to, msg)
-
-    def render_msg(self, frm, to, subject, content, attachments):
-        msg = MIMEMultipart()
-        msg['Subject'] = subject
-        msg['To'] = ', '.join(to)
-        msg['Date'] = str(date.today())
-        msg['From'] = frm
-        msg.attach(MIMEText(content))
-        msg = self.append_attachments(msg, attachments)
-
-        return msg.as_string()
-        
-    def append_attachments(self, msg, attachments):
-        for attachment in attachments:
-            try:
-                with open(attachment, 'rb') as fp:
-                    file = MIMEBase('application', "octet-stream")
-                    file.set_payload(fp.read())
-                encoders.encode_base64(file)
-                file.add_header('Content-Disposition', 'attachment',
-                filename=os.path.basename(attachment))
-                msg.attach(file)
-            except:
-                print("Unable to open one of the attachments. Error: ", sys.exc_info()[0])
-                raise
-        
-        return msg
+    def send(self, frm, to, content):
+        """
+        <to> must be a list
+        """
+        self.smtp_obj.sendmail(frm, to, content)
 
     def quit(self):
+        """
+        Terminate the <smtp_obj> session
+        """
         self.smtp_obj.quit()
