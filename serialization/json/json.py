6dc0f591d52c33f8767cd3b3fb410119f7923817
莊雲凱 <as23041248@gmail.com>
2017-10-06 10:26:42 +0800

    [Serialization] Impl json Serializer
    The serializer in Json convert python dict to string(raw data),
    and deserializer convert string(raw data) to python dict.

---
+++ serialization/json/json.py
@@ -0,0 +1,14 @@
+import json
+
+
+class Json:
+
+    def serialize(data):
+        if type(data) is dict:
+            return json.dumps(data)
+
+    def deserialize(data):
+        if type(data) is str:
+            return json.loads(data)
+
+
