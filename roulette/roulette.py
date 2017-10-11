21554313a1894599a780647706574efc9739a9e4
Yen-Kuan Wu <c14006078@gmail.com>
2017-10-11 20:35:40 +0800

    roulette: Tweak class mem attr from priv to rdonly
    It's OK to expose policy and candiates of roulette object to users.
    But no write permission for those, users should call `setUp` method
    to update the configuration.

--- roulette/roulette.py
+++ roulette/roulette.py
@@ -1,9 +1,9 @@
 class Roulette():
     def __init__(self, policy, cands = ()):
         # Declaration
-        self.__policy = None
-        self.__candidates = None
-        self.__ptr = None
+#        self.__policy = None
+#        self.__candidates = None
+#        self.__ptr = None
 
         # Initialization
         self.setUp(policy, cands)
@@ -16,3 +16,11 @@ class Roulette():
     def lottery(self):
         self.__ptr = self.__policy(self.__ptr, self.__candidates)
         return self.__ptr
+
+    @property
+    def policy(self):
+        return self.__policy
+
+    @property
+    def candidates(self):
+        return self.__candidates
