eb7b12a105fdaee287ceb8267e80d83ec842ab39
Yen-Kuan Wu <yenwu.tw@gmail.com>
2017-10-15 18:12:19 +0800

    serializer: Extract `setUp()` for initialization

--- serializer/serializer.py
+++ serializer/serializer.py
@@ -1,5 +1,12 @@
 class Serializer:
     def __init__(self, serialize_obj):
+        # Declaration
+        self.serialize_obj = None
+
+        # Initialization
+        self.setUp(serialize_obj)
+    
+    def setUp(self, serialize_obj):
         self.serialize_obj = serialize_obj
 
     def serialize(self, data):
