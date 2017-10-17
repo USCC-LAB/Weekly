7d4b5f72c965dbeceeb99928f1728bacf6c696d5
Yen-Kuan Wu <yenwu.tw@gmail.com>
2017-10-18 03:07:39 +0800

    serializer, test: Extract duplicate initialization

--- tests/testsuites/serializer/test_json.py
+++ tests/testsuites/serializer/test_json.py
@@ -2,11 +2,11 @@ from serializer import serializer
 from serializer.prot import jsonlib
 
 
+Serializer = serializer.Serializer(jsonlib.Json())
+
 def json_serialize(dict_data):
-    Serializer = serializer.Serializer(jsonlib.Json())
     return Serializer.dumps(dict_data)
 
 
 def json_deserialize(raw_data):
-    Serializer = serializer.Serializer(jsonlib.Json())
     return Serializer.loads(raw_data)
