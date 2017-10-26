0145b9f4bf320b5c5832ccc1bba186a66cb9ad07
3rzx <3333rrrr@gmail.com>
2017-10-26 14:57:20 +0800

    DB: Introduce generic DB interface.

---
+++ DB/DB.py
@@ -0,0 +1,21 @@
+class DB:
+	def __init__(self,db):
+		# Declaration
+        self.__db = None
+
+        # Initialization
+        self.__db = db
+
+
+	def CreateTable(self):
+		self.__db.createTable()
+
+	def insertStudent(self,dbname,table,sid,name,cardId):
+		self.__db.insertStudent(dbname,table,sid,name,cardId)
+
+	def checkIn(self,dbname,table,cardId): 
+		self.__db.checkIn(cardId)
+	
+	def checkOut(self,dbname,table,cardId): 
+		self.__db.checkOut(cardId)
+
